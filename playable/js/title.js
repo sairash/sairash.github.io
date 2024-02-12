b75907f391e43ece1f16b57901a6fe974c23efed
sairash <gautamsairash@gmail.com>
2024-02-12 15:22:31 +0545

    Title Updates

--- playable/js/title.js
+++ playable/js/title.js
@@ -1,23 +1,30 @@
 totalString = 37
 currentPostion = 1
 placeholderString = '-'
+timeout = false
 
 function titleShow(){
     document.title= placeholderString.repeat(currentPostion - 1) + '🐰' + placeholderString.repeat(totalString - currentPostion - 1)
 }
 
 function changeCurrentPosition(direction){
-    if(direction == 'left'){
-        currentPostion -= 1;
-    }else{
-        currentPostion += 1;
-    }
-    if(currentPostion <= 0){
-        currentPostion = totalString - 1
-    }
-    if(currentPostion >= totalString){
-        currentPostion = 1
+    if(!timeout){
+        if(direction == 'left'){
+            currentPostion -= 1;
+        }else{
+            currentPostion += 1;
+        }
+        if(currentPostion <= 0){
+            currentPostion = totalString - 1
+        }
+        if(currentPostion >= totalString){
+            currentPostion = 1
+        }
+        timeout = true
+        setTimeout(()=>{
+            timeout = false
+            titleShow()
+        }, 100)
     }
 
-    titleShow()
 }
